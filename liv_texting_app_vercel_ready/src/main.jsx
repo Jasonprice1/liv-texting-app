3cc27e91f6ff683f9ca495cdada1ad3608e893ab
Jasonprice1 <jasonbprice@hotmail.com>
2025-04-15 20:36:44 -0700

    Add files via upload

---
+++ liv_texting_app_vercel_ready/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ChatMockup from './ChatMockup.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChatMockup />
+  </React.StrictMode>
+);
