3694f0fe624178483cedadf16c5bc91f4c5725c6
Jasonprice1 <jasonbprice@hotmail.com>
2025-04-15 20:09:51 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()]
-});
+// Simplified config for plain Vite React project
+export default {
+  build: {
+    outDir: 'dist'
+  }
+};
